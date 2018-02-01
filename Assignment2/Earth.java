2d34b6ab662e76caf3440ef2d5d791269857ba5b
pravallikathappeta <35616224+pravallikathappeta@users.noreply.github.com>
2018-02-01 11:01:40 -0800

    Create Earth.java

---
+++ Assignment2/Earth.java
@@ -0,0 +1,14 @@
+package com.pavi;
+
+public class Earth {
+    public static void main(String args[]){
+        Human pravallika;
+        pravallika= new Human();
+        pravallika.age=22;
+        pravallika.eyeColor= "black";
+        pravallika.name="Pravallika Reddy";
+        pravallika.height=53;
+        pravallika.speak();
+    }
+
+}
