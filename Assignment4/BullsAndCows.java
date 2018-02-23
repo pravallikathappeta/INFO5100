157f8968b9bc42f70eb98980c38073a502bf354b
annlearner <31709240+annlearner@users.noreply.github.com>
2018-02-23 15:09:45 -0800

    Update BullsAndCows.java

--- Assignment4/BullsAndCows.java
+++ Assignment4/BullsAndCows.java
@@ -1,3 +1,63 @@
+nice try, but I suggest you use a clear logic to solve the problem , please look at my answer and learn it:
+
+ public String getHint(String secret, String guess) {
+        if(guess.equals("") || guess.length() == 0){
+            return "";
+        }
+        int countA = 0, countB = 0;
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char ch : secret.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0) + 1);
+        }
+        for(int i = 0 ;i < secret.length(); i++){
+            if(secret.charAt(i) == guess.charAt(i)){
+                countA++;
+                map.put(secret.charAt(i), map.get(secret.charAt(i)) - 1);
+            }
+        } 
+        for(int i = 0 ;i < secret.length(); i++){
+            if(secret.charAt(i) != guess.charAt(i) && map.containsKey(guess.charAt(i))){
+                if(map.get(guess.charAt(i)) > 0){
+                    map.put(guess.charAt(i), map.get(guess.charAt(i)) - 1);
+                    countB++;
+            }  
+        }
+        }
+        return countA + "A" + countB + "B";
+    }
+
+
+more simple approach: 
+
+public String getHint(String secret, String guess) {
+    int bulls = 0;
+    int cows = 0;
+    int[] numbers = new int[10];
+    for (int i = 0; i<secret.length(); i++) {
+        int s = Character.getNumericValue(secret.charAt(i));
+        int g = Character.getNumericValue(guess.charAt(i));
+        if (s == g) bulls++;
+        else {
+            if (numbers[s] < 0) cows++;
+            if (numbers[g] > 0) cows++;
+            numbers[s] ++;
+            numbers[g] --;
+        }
+    }
+    return bulls + "A" + cows + "B";
+}
+
+
+scores:  1.5 / 2.0
+
+
+
+
+
+
+
+
+
 import java.util.Random;
 import java.util.Scanner;
 import java.util.regex.Pattern;
