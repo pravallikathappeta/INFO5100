ff1164514bb47cf866b84a2132697cdd9944ce07
annlearner <31709240+annlearner@users.noreply.github.com>
2018-02-23 15:03:52 -0800

    Update question2.java

--- Assignment4/question2.java
+++ Assignment4/question2.java
@@ -1,3 +1,16 @@
+feedback: 
+1. keep main function short and write the logic in the method function.
+2. in the map, you use index as key and the elements as value. if you want to delete one element, we always search it by map.get(element)
+so you should keep the value in the key postion, and count the time in the value part.
+3. about delete function, the requirement is delete the element 6,8. You shouldn't assume you know their index, then delete it.
+4. please pay attention to coding style. use if(){} else{}.
+5. give return value and system print the function in "main".
+  
+    scores : 1.5 / 2
+
+
+
+
 package com.pavi;
 
 import java.util.HashMap;
