cf4753426daceb19d6b0df635dfe2f6749655e93
annlearner <31709240+annlearner@users.noreply.github.com>
2018-03-02 18:12:15 -0800

    Update Solution.java

--- Assignment5/Solution.java
+++ Assignment5/Solution.java
@@ -1,3 +1,35 @@
+// scores : 1 / 2
+//where is the main function? this code did not run properly because of logic problem. you should use stack to solve this problem.
+// if my input is "[{}]", your answer is wrong
+//here is what i suggest :
+
+// public class IsValidBrackets {
+//     public static boolean isValidBrackets(String s) {
+//         Stack<Character> stack = new Stack<>();
+//         for (int i = 0; i < s.length(); i++) {
+//             char c = s.charAt(i);
+//             if (c == '(') {
+//                 stack.push(')');
+//             } else if (c == '[') {
+//                 stack.push(']');
+//             } else if (c == '{') {
+//                 stack.push('}');
+//             } else if (stack.isEmpty() || stack.peek() != c) {
+//                 return false;
+//             } else {
+//                 stack.pop();
+//             }
+//         }
+//         return stack.isEmpty();
+//     }
+
+//     public static void main(String[] args) {
+//         System.out.println(isValidBrackets("([]){}"));
+//     }
+// }
+//
+
+
 public class Solution
 {
     public boolean isValid(String s)
